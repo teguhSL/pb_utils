e2cefe45d34ba03709e239954b5b9456a882f172
Teguh Santoso Lembono <tarsle14@gmail.com>
2022-01-18 18:07:35 +0100

    Add files to the lib

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+# Setup:
+setup(name='pb_utils',
+      version='0.1',
+      description='Pybullet utility functions',
+      url='',
+      author='Teguh Santoso Lembono',
+      author_email='teguh.lembono@idiap.ch',
+      license='MIT',
+      packages=find_packages(),
+      install_requires = [],
+      zip_safe=False)
