01f3b2ffea90042acf8b899477c6ce882bcc6fb0
Teguh Santoso Lembono <tarsle14@gmail.com>
2022-03-17 18:02:06 +0100

    Include plot_compare

--- pb_utils/visualize.py
+++ pb_utils/visualize.py
@@ -1,24 +1,25 @@
 import pybullet as p
 import numpy as np
 import time
-import pyscreenshot as ImageGrab
+import matplotlib.pyplot as plt
+#import pyscreenshot as ImageGrab
 
-def save_screenshot(x,y,w,h,file_name, to_show='False'):
-    # part of the screen
-    im=ImageGrab.grab(bbox=(x,y,w,h))
-    if to_show:
-        im.show()
-
-    # save to file
-    im.save(file_name)
-    return im
+#def save_screenshot(x,y,w,h,file_name, to_show='False'):
+#    # part of the screen
+#    im=ImageGrab.grab(bbox=(x,y,w,h))
+#    if to_show:
+#        im.show()
+#
+#    # save to file
+#    im.save(file_name)
+#    return im
 
 #Kinematics functions
 
 def normalize(x):
     return x/np.linalg.norm(x)
 
-def set_q(robot_id, joint_indices, q, set_base = False):
+def set_q(q, robot_id, joint_indices,  set_base = False):
     if set_base:
         localInertiaPos = np.array(p.getDynamicsInfo(robot_id,-1)[3])
         q_root = q[0:7]
@@ -60,4 +61,22 @@ def convexify_obj(name_in, name_out, name_log='log.txt', resolution = 10000000, 
     name_out = rl.datapath + '/urdf/bookcase.obj'
     '''
     p.vhacd(name_in, name_out, name_log, alpha=0.04,resolution=resolution )
+
+def plot_compare(nrows, ncols, datas, colors, labels, titles=None, filename=None):
+    '''
+    Plot & compare a number of data(s) over the same range
+    '''
+    fig,axs = plt.subplots(nrows, ncols)
+    fig.set_size_inches(ncols*4, nrows*4)
+    D = datas[0].shape[1]
+    for i in range(D):
+        for j,data in enumerate(datas):
+            axs.flatten()[i].plot(data[:,i], colors[j], label=labels[j])
+        if titles is not None:
+            axs.flatten()[i].set_title(titles[i])
+    axs[0,0].legend()
+    if filename is not None:
+        plt.savefig(filename, dpi=200, facecolor="w")
+    plt.show()
+    return fig
 
